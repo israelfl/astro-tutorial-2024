46be53b829f50e1194725d4d4f922086f346ca2a
Israel Falcón Leal <ifldeveloer@gmail.com>
2024-11-16 00:32:27 +0100

    Tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+   site: "https://earnest-flan-223d1a.netlify.app"
+});
